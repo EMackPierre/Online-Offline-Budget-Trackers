095bc9232d3b4ae1bcde113866d8c5a01aa0627d
Ernst Pierre <emmp318@gmail.com>
2020-09-15 17:57:40 -0500

    modified db.js and service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,8 @@
+let db;
+// create a new db request for a "budget" database.
+const request = indexedDB.open("bdget", 1);
+
+request.onupgradeneeded = function(event) {
+    //create object store called "pending" and set autoIncrement to true
+    const db = event.target
+}
